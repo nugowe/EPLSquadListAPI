f2ce0cdd5910accc0f7adac4bb2f20647a620557
nugowe <nosa.ugowe@yahoo.com>
2024-01-11 18:10:28 -0600

    Made changes to the R scrape code

--- EPLROASTERLIST.R
+++ EPLROASTERLIST.R
@@ -7,25 +7,11 @@ library(rvest)
 
 #THE DETAILS ARE SCRAPED BASED ON THE HTML PAGE DOM DETAILS AND CLEANED UP IN TO A DATAFRAME.
 
-print("SCRAPING DETAILS FOR THE ARSENAL SQUAD LIST...")
-
-ArsenalUrl <- 'https://en.wikipedia.org/wiki/Arsenal_F.C.'
-session = bow(user_agent = "arsenal-scrape", ArsenalUrl)
-ArsenalTable <- scrape(session) %>% html_nodes("table:nth-child(131)") %>% html_table()
-ArsenalTable <- as.data.frame(ArsenalTable)
-ArsenalTable <- ArsenalTable[-c(1,16),-c(1)] %>% select(c(1:3))
-names(ArsenalTable)[1] <- "Position"
-names(ArsenalTable)[2] <- "Nation"
-names(ArsenalTable)[3] <- "PlayerName"
-ArsenalTable <- ArsenalTable[-c(1),]
-ArsenalTable$Team <- "Arsenal"
-
-
 print("SCRAPING DETAILS FOR THE CHELSEA SQUAD LIST...")
 
 ChelseaUrl <- 'https://en.wikipedia.org/wiki/Chelsea_F.C.'
-ChelseaTable <- scrape(session) %>% html_nodes("table:nth-child(141)") %>% html_table()
 session = bow(user_agent = "Chelsea-Scrape", ChelseaUrl)
+ChelseaTable <- scrape(session) %>% html_nodes("table:nth-child(142)") %>% html_table()
 ChelseaTable <- as.data.frame(ChelseaTable)
 ChelseaTable <- ChelseaTable[-c(1,18),-c(1)] %>% select(c(1:3))
 names(ChelseaTable)[1] <- "Position"
@@ -34,11 +20,21 @@ names(ChelseaTable)[3] <- "PlayerName"
 ChelseaTable <- ChelseaTable[-c(1),]
 ChelseaTable$Team <- "Chelsea"
 
-print("SCRAPING DETAILS FOR THE LIVERPOOL SQUAD LIST...")
+ArsenalUrl <- 'https://en.wikipedia.org/wiki/Arsenal_F.C.'
+session = bow(user_agent = "arsenal-scrape", ArsenalUrl)
+ArsenalTable <- scrape(session) %>% html_nodes("table:nth-child(133)") %>% html_table()
+ArsenalTable <- as.data.frame(ArsenalTable)
+ArsenalTable <- ArsenalTable[-c(1,16),-c(1)] %>% select(c(1:3))
+names(ArsenalTable)[1] <- "Position"
+names(ArsenalTable)[2] <- "Nation"
+names(ArsenalTable)[3] <- "PlayerName"
+ArsenalTable <- ArsenalTable[-c(1),]
+ArsenalTable$Team <- "Arsenal"
 
-LiverpoolUrl <- 'https://en.wikipedia.org/wiki/Liverpool_F.C.'
-LiverpoolTable <- scrape(session) %>% html_nodes("table:nth-child(82)") %>% html_table()
+LiverpoolUrl <- 'https://en.wikipedia.org/wiki/Liverpool_F.C.?wprov=srpw1_0'
 session = bow(user_agent = "Liverpool-Scrape", LiverpoolUrl)
+LiverpoolTable <- scrape(session) %>% html_nodes("table:nth-child(83)") %>% html_table()
+
 LiverpoolTable <- as.data.frame(LiverpoolTable)
 LiverpoolTable <- LiverpoolTable[-c(1,17),-c(1)] %>% select(c(1:3))
 names(LiverpoolTable)[1] <- "Position"
@@ -47,11 +43,10 @@ names(LiverpoolTable)[3] <- "PlayerName"
 LiverpoolTable <- LiverpoolTable[-c(1),]
 LiverpoolTable$Team <- "Liverpool"
 
-print("SCRAPING DETAILS FOR THE MANCHESTER CITY SQUAD LIST...")
-
 ManchesterCityUrl <- 'https://en.wikipedia.org/wiki/Manchester_City_F.C.'
-ManchesterCityTable <- scrape(session) %>% html_nodes("table:nth-child(71)") %>% html_table()
 session = bow(user_agent = "ManchesterCity-Scrape", ManchesterCityUrl)
+ManchesterCityTable <- scrape(session) %>% html_nodes("table:nth-child(70)") %>% html_table()
+
 ManchesterCityTable <- as.data.frame(ManchesterCityTable)
 ManchesterCityTable <- ManchesterCityTable[-c(1,15),-c(1)] %>% select(c(1:3))
 names(ManchesterCityTable)[1] <- "Position"
@@ -60,11 +55,10 @@ names(ManchesterCityTable)[3] <- "PlayerName"
 ManchesterCityTable <- ManchesterCityTable[-c(1),]
 ManchesterCityTable$Team <- "ManchesterCity"
 
-print("SCRAPING DETAILS FOR THE MANCHESTER UNITED SQUAD LIST...")
-
 ManchesterUnitedUrl <- 'https://en.wikipedia.org/wiki/Manchester_United_F.C.'
-ManchesterUnitedTable <- scrape(session) %>% html_nodes("table:nth-child(137)") %>% html_table()
 session = bow(user_agent = "ManchesterUnited-Scrape", ManchesterUnitedUrl)
+ManchesterUnitedTable <- scrape(session) %>% html_nodes("table:nth-child(136)") %>% html_table()
+
 ManchesterUnitedTable <- as.data.frame(ManchesterUnitedTable)
 ManchesterUnitedTable <- ManchesterUnitedTable[-c(1,19),-c(1)] %>% select(c(1:3))
 names(ManchesterUnitedTable)[1] <- "Position"
@@ -74,11 +68,10 @@ ManchesterUnitedTable <- ManchesterUnitedTable[-c(1),]
 ManchesterUnitedTable$Team <- "ManchesterUnited"
 
 
-print("SCRAPING DETAILS FOR THE EVERTON SQUAD LIST...")
-
 EvertonUrl <- 'https://en.wikipedia.org/wiki/Everton_F.C.'
-EvertonTable <- scrape(session) %>% html_nodes("table:nth-child(89)") %>% html_table()
 session = bow(user_agent = "Everton-Scrape", EvertonUrl)
+EvertonTable <- scrape(session) %>% html_nodes("table:nth-child(89)") %>% html_table()
+
 EvertonTable <- as.data.frame(EvertonTable)
 EvertonTable <- EvertonTable[-c(1,16),-c(1)] %>% select(c(1:3))
 names(EvertonTable)[1] <- "Position"
@@ -87,11 +80,11 @@ names(EvertonTable)[3] <- "PlayerName"
 EvertonTable <- EvertonTable[-c(1),]
 EvertonTable$Team <- "Everton"
 
-print("SCRAPING DETAILS FOR THE BRIGHTON SQUAD LIST...")
 
 BrightonUrl <- 'https://en.wikipedia.org/wiki/Brighton_%26_Hove_Albion_F.C.'
-BrightonTable <- scrape(session) %>% html_nodes("table:nth-child(64)") %>% html_table()
 session = bow(user_agent = "Brighton-Scrape", BrightonUrl)
+BrightonTable <- scrape(session) %>% html_nodes("table:nth-child(64)") %>% html_table()
+
 BrightonTable <- as.data.frame(BrightonTable)
 BrightonTable <- BrightonTable[-c(1,16),-c(1)] %>% select(c(1:3))
 names(BrightonTable)[1] <- "Position"
@@ -100,11 +93,11 @@ names(BrightonTable)[3] <- "PlayerName"
 BrightonTable <- BrightonTable[-c(1),]
 BrightonTable$Team <- "Brighton"
 
-print("SCRAPING DETAILS FOR BRENTFORD SQAUD LIST....")
 
 BrentfordUrl <- 'https://en.wikipedia.org/wiki/Brentford_F.C.'
-BrentfordTable <- scrape(session) %>% html_nodes("table:nth-child(50)") %>% html_table()
 session = bow(user_agent = "Brentford-Scrape", BrentfordUrl)
+BrentfordTable <- scrape(session) %>% html_nodes("table:nth-child(50)") %>% html_table()
+
 BrentfordTable <- as.data.frame(BrentfordTable)
 BrentfordTable <- BrentfordTable[-c(1,17),-c(1)] %>% select(c(1:3))
 names(BrentfordTable)[1] <- "Position"
@@ -113,11 +106,10 @@ names(BrentfordTable)[3] <- "PlayerName"
 BrentfordTable <- BrentfordTable[-c(1),]
 BrentfordTable$Team <- "Brentford"
 
-print("SCRAPING DETAILS FOR THE TOTTENHAMHOTSPUR SQUAD LIST...")
-
 TottenhamHotspurUrl <- 'https://en.wikipedia.org/wiki/Tottenham_Hotspur_F.C.'
-TottenhamHotspurTable <- scrape(session) %>% html_nodes("table:nth-child(144)") %>% html_table()
 session = bow(user_agent = "TottenhamHotspur-Scrape", TottenhamHotspurUrl)
+TottenhamHotspurTable <- scrape(session) %>% html_nodes("table:nth-child(144)") %>% html_table()
+
 TottenhamHotspurTable <- as.data.frame(TottenhamHotspurTable)
 TottenhamHotspurTable <- TottenhamHotspurTable[-c(1,18),-c(1)] %>% select(c(1:3))
 names(TottenhamHotspurTable)[1] <- "Position"
@@ -126,7 +118,6 @@ names(TottenhamHotspurTable)[3] <- "PlayerName"
 TottenhamHotspurTable <- TottenhamHotspurTable[-c(1),]
 TottenhamHotspurTable$Team <- "TottenhamHotspur"
 
-print("SCRAPING DETAILS FOR THE WESTHAM SQUAD LIST...")
 
 WesthamUrl <- 'https://en.wikipedia.org/wiki/West_Ham_United_F.C.'
 WesthamTable <- scrape(session) %>% html_nodes("table:nth-child(155)") %>% html_table()
@@ -139,11 +130,11 @@ names(WesthamTable)[3] <- "PlayerName"
 WesthamTable <- WesthamTable[-c(1),]
 WesthamTable$Team <- "WestHam"
 
-print("SCRAPING DETAILS FOR THE ASTON VILLA SQUAD LIST...")
 
 AstonVillaUrl <- 'https://en.wikipedia.org/wiki/Aston_Villa_F.C.'
-AstonVillaTable <- scrape(session) %>% html_nodes("table:nth-child(139)") %>% html_table()
 session = bow(user_agent = "AstonVilla-Scrape", AstonVillaUrl)
+AstonVillaTable <- scrape(session) %>% html_nodes("table:nth-child(141)") %>% html_table()
+
 AstonVillaTable <- as.data.frame(AstonVillaTable)
 AstonVillaTable <- AstonVillaTable[-c(1,16),-c(1)] %>% select(c(1:3))
 names(AstonVillaTable)[1] <- "Position"
@@ -152,11 +143,10 @@ names(AstonVillaTable)[3] <- "PlayerName"
 AstonVillaTable <- AstonVillaTable[-c(1),]
 AstonVillaTable$Team <- "AstonVilla"
 
-print("SCRAPING DETAILS FOR THE WOLVES SQUAD LIST...")
-
 WolverhamptonWanderersUrl <- 'https://en.wikipedia.org/wiki/Wolverhampton_Wanderers_F.C.'
-WolverhamptonWanderersTable <- scrape(session) %>% html_nodes("table:nth-child(80)") %>% html_table()
 session = bow(user_agent = "WolverhamptonWanderers-Scrape", WolverhamptonWanderersUrl)
+WolverhamptonWanderersTable <- scrape(session) %>% html_nodes("table:nth-child(80)") %>% html_table()
+
 WolverhamptonWanderersTable <- as.data.frame(WolverhamptonWanderersTable)
 WolverhamptonWanderersTable <- WolverhamptonWanderersTable[-c(1,16),-c(1)] %>% select(c(1:3))
 names(WolverhamptonWanderersTable)[1] <- "Position"
@@ -165,11 +155,12 @@ names(WolverhamptonWanderersTable)[3] <- "PlayerName"
 WolverhamptonWanderersTable <- WolverhamptonWanderersTable[-c(1),]
 WolverhamptonWanderersTable$Team <- "WolverhamptonWanderers"
 
-print("SCRAPING DETAILS FOR THE BURNLEY SQUAD LIST...")
+
 
 BurnleyUrl <- 'https://en.wikipedia.org/wiki/Burnley_F.C.'
-BurnleyTable <- scrape(session) %>% html_nodes("table:nth-child(75)") %>% html_table()
 session = bow(user_agent = "Burnley-Scrape", BurnleyUrl)
+BurnleyTable <- scrape(session) %>% html_nodes("table:nth-child(75)") %>% html_table()
+
 BurnleyTable <- as.data.frame(BurnleyTable)
 BurnleyTable <- BurnleyTable[-c(1,20),-c(1)] %>% select(c(1:3))
 names(BurnleyTable)[1] <- "Position"
@@ -178,11 +169,11 @@ names(BurnleyTable)[3] <- "PlayerName"
 BurnleyTable <- BurnleyTable[-c(1),]
 BurnleyTable$Team <- "Burnley"
 
-print("SCRAPING DETAILS FOR CRYSTAL PALACE SQUAD LIST...")
 
 CrystalPalaceUrl <- 'https://en.wikipedia.org/wiki/Crystal_Palace_F.C.'
-CrystalPalaceTable <- scrape(session) %>% html_nodes("table:nth-child(112)") %>% html_table()
 session = bow(user_agent = "CrystalPalace-Scrape", CrystalPalaceUrl)
+CrystalPalaceTable <- scrape(session) %>% html_nodes("table:nth-child(112)") %>% html_table()
+
 CrystalPalaceTable <- as.data.frame(CrystalPalaceTable)
 CrystalPalaceTable <- CrystalPalaceTable[-c(1,17),-c(1)] %>% select(c(1:3))
 names(CrystalPalaceTable)[1] <- "Position"
@@ -191,11 +182,11 @@ names(CrystalPalaceTable)[3] <- "PlayerName"
 CrystalPalaceTable <- CrystalPalaceTable[-c(1),]
 CrystalPalaceTable$Team <- "CrystalPalace"
 
-print("SCRAPING DETAILS FOR FULHAM SQUAD LIST...")
 
 FulhamUrl <- 'https://en.wikipedia.org/wiki/Fulham_F.C.'
-FulhamTable <- scrape(session) %>% html_nodes("table:nth-child(113)") %>% html_table()
 session = bow(user_agent = "fulham-test-intern", FulhamUrl)
+FulhamTable <- scrape(session) %>% html_nodes("table:nth-child(113)") %>% html_table()
+
 FulhamTable <- as.data.frame(FulhamTable)
 FulhamTable <- FulhamTable[-c(1),-c(1)] %>% select(c(1:3))
 names(FulhamTable)[1] <- "Position"
@@ -204,11 +195,12 @@ names(FulhamTable)[3] <- "PlayerName"
 FulhamTable <- FulhamTable[-c(1),]
 FulhamTable$Team <- "Fulham"
 
-print("SCRAPING DETAILS FOR LUTON SQUAD LIST...")
 
 LutonUrl <- 'https://en.wikipedia.org/wiki/Luton_F.C.'
-LutonTable <- scrape(session) %>% html_nodes("table:nth-child(87)") %>% html_table()
+
 session = bow(user_agent = "Luton-test-intern", LutonUrl)
+LutonTable <- scrape(session) %>% html_nodes("table:nth-child(87)") %>% html_table()
+
 LutonTable <- as.data.frame(LutonTable)
 LutonTable <- LutonTable[-c(1),-c(1)] %>% select(c(1:3))
 names(LutonTable)[1] <- "Position"
@@ -217,11 +209,10 @@ names(LutonTable)[3] <- "PlayerName"
 LutonTable <- LutonTable[-c(1),]
 LutonTable$Team <- "Luton"
 
-print("SCRAPING DETAILS FOR SHEFFIELD UNITED SQUAD LIST...")
-
 SheffieldUnitedUrl <- 'https://en.wikipedia.org/wiki/Sheffield_United_F.C.'
-SheffieldUnitedTable <- scrape(session) %>% html_nodes("table:nth-child(89)") %>% html_table()
 session = bow(user_agent = "SheffieldUnited-test-intern", SheffieldUnitedUrl)
+SheffieldUnitedTable <- scrape(session) %>% html_nodes("table:nth-child(90)") %>% html_table()
+
 SheffieldUnitedTable <- as.data.frame(SheffieldUnitedTable)
 SheffieldUnitedTable <- SheffieldUnitedTable[-c(1),-c(1)] %>% select(c(1:3))
 names(SheffieldUnitedTable)[1] <- "Position"
@@ -230,11 +221,10 @@ names(SheffieldUnitedTable)[3] <- "PlayerName"
 SheffieldUnitedTable <- SheffieldUnitedTable[-c(1),]
 SheffieldUnitedTable$Team <- "SheffieldUnited"
 
-print("SCRAPING DETAILS FOR BOURNEMOUTH SQUAD LIST...")
-
 BournemouthUrl <- 'https://en.wikipedia.org/wiki/AFC_Bournemouth'
-BournemouthTable <- scrape(session) %>% html_nodes("table:nth-child(68)") %>% html_table()
 session = bow(user_agent = "Bournemouth-test-intern", BournemouthUrl)
+BournemouthTable <- scrape(session) %>% html_nodes("table:nth-child(68)") %>% html_table()
+
 BournemouthTable <- as.data.frame(BournemouthTable)
 BournemouthTable <- BournemouthTable[-c(1),-c(1)] %>% select(c(1:3))
 names(BournemouthTable)[1] <- "Position"
@@ -243,11 +233,9 @@ names(BournemouthTable)[3] <- "PlayerName"
 BournemouthTable <- BournemouthTable[-c(1),]
 BournemouthTable$Team <- "Bournemouth"
 
-print("SCRAPING DETAILS FOR NEWSCASTLE UNITED SQUAD LIST...")
-
 NewscastleUnitedUrl <- 'https://en.wikipedia.org/wiki/Newcastle_United_F.C.'
-NewscastleUnitedTable <- scrape(session) %>% html_nodes("table:nth-child(130)") %>% html_table()
 session = bow(user_agent = "NewscastleUnited-test-intern", NewscastleUnitedUrl)
+NewscastleUnitedTable <- scrape(session) %>% html_nodes("table:nth-child(130)") %>% html_table()
 NewscastleUnitedTable <- as.data.frame(NewscastleUnitedTable)
 NewscastleUnitedTable <- NewscastleUnitedTable[-c(1),-c(1)] %>% select(c(1:3))
 names(NewscastleUnitedTable)[1] <- "Position"
@@ -256,13 +244,11 @@ names(NewscastleUnitedTable)[3] <- "PlayerName"
 NewscastleUnitedTable <- NewscastleUnitedTable[-c(1),]
 NewscastleUnitedTable$Team <- "NewscastleUnited"
 
-print("SCRAPING DETAILS FOR NOTTINGHAM FOREST SQUAD LIST...")
-
 NottinghamforestUrl <- 'https://en.wikipedia.org/wiki/Nottingham_Forest_F.C.'
-NottinghamforestTable <- scrape(session) %>% html_nodes("table:nth-child(172)") %>% html_table()
 session = bow(user_agent = "Nottinghamforest-test-intern", NottinghamforestUrl)
+NottinghamforestTable <- scrape(session) %>% html_nodes("table:nth-child(152)") %>% html_table()
 NottinghamforestTable <- as.data.frame(NottinghamforestTable)
-NottinghamforestTable <- NottinghamforestTable[-c(1),-c(1)] %>% select(c(1:3))
+NottinghamforestTable <- NottinghamforestTable[-c(1)] %>% select(c(1:3))
 names(NottinghamforestTable)[1] <- "Position"
 names(NottinghamforestTable)[2] <- "Nation"
 names(NottinghamforestTable)[3] <- "PlayerName"
